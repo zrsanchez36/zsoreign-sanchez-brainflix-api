2a02ffb5bded1bf70749c589cbb00dbe41b1f4f7
Zsoreign <zrsanchez36@gmail.com>
2023-09-27 16:40:13 -0400

    Added api functionality

--- routes/videos.js
+++ routes/videos.js
@@ -3,52 +3,65 @@ const bodyParser = require('body-parser');
 const uuid = require('uuid');
 const fs = require('fs');
 const path = require('path');
+const cors = require('cors');
+
 const router = express.Router();
 
-const videosPath = path.join(__dirname, '..', 'data', 'videos.json'); // Adjusted the path
+const videos = require('../data/videos.json');
+const videosPath = path.join(__dirname, '..', 'data', 'videos.json');
 
+router.use(cors({
+    origin: 'http://localhost:3000',
+}));
 router.use(bodyParser.json());
-router.use('/images', express.static('./public/images')); // Adjust this path if needed
+router.use('/public/images', express.static(path.join(__dirname, '..', 'public', 'images')));
 
-// GET /videos
 router.get('/', (req, res) => {
-    let videos = JSON.parse(fs.readFileSync(videosPath, 'utf-8'));
     res.json(videos.map(video => ({
         id: video.id,
         title: video.title,
+        channel: video.channel,
         image: video.image,
     })));
 });
 
-// GET /videos/:id
 router.get('/:id', (req, res) => {
-    let videos = JSON.parse(fs.readFileSync(videosPath, 'utf-8'));
     const video = videos.find(v => v.id === req.params.id);
     if (video) {
         res.json(video);
     } else {
-        res.status(404).send('Video not found');
+        res.status(404).send({ message: 'Video not found' });
     }
 });
 
-// POST /videos
 router.post('/', (req, res) => {
     const { title, description } = req.body;
-    if (!title || !description) {
-        return res.status(400).send('Title and Description are required');
-    }
-
-    let videos = JSON.parse(fs.readFileSync(videosPath, 'utf-8'));
+    if (!title || !description) return res.status(400).send('Title and Description are required');
+    
     const newVideo = {
         id: uuid.v4(),
         title,
         description,
         image: '/images/default-image.jpg',
+        comments: [],
     };
     videos.push(newVideo);
     fs.writeFileSync(videosPath, JSON.stringify(videos, null, 4));
-
     res.status(201).json(newVideo);
+});
+
+router.post('/:id/comments', (req, res) => {
+    const { id } = req.params;
+    const { name, comment } = req.body;
+    
+    const video = videos.find(v => v.id === id);
+    if (!video) return res.status(404).send({ message: 'Video not found' });
+    
+    const newComment = { name, comment, id: uuid.v4(), timestamp: Date.now() };
+    video.comments.push(newComment);
+    
+    fs.writeFileSync(videosPath, JSON.stringify(videos, null, 4));
+    res.status(201).json(newComment);
 });
 
 module.exports = router;
--- server.js
+++ server.js
@@ -24,23 +24,51 @@
 //     console.log('Press CTRL + C to stop server');
 // });
 
+// const express = require('express');
+// const cors = require('cors');
+// const path = require('path');
+// const videosRoute = require('./routes/videos');
+
+// const app = express();
+
+// // Setup middlewares
+// app.use(express.json());
+// app.use(cors({ origin: 'http://localhost:3000' }));
+// app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+
+// // Setup route
+// app.use('/videos', videosRoute);
+
+// // start Express on port 5000
+// app.listen(5000, () => {
+//     console.log('Server Started on http://localhost:5000');
+//     console.log('Press CTRL + C to stop server');
+// });
+
+
 const express = require('express');
 const cors = require('cors');
-const path = require('path');
+const bodyParser = require('body-parser');
 const videosRoute = require('./routes/videos');
+const dotenv = require('dotenv');
+dotenv.config();
 
 const app = express();
 
-// Setup middlewares
+const CORS_ORIGIN = process.env.CORS_ORIGIN || 'http://localhost:3000';
+
+app.use(cors({
+    origin: CORS_ORIGIN,
+}));
+
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' }));
-app.use('/images', express.static(path.join(__dirname, 'public', 'images')));
+app.use(bodyParser.json());
+app.use('/images', express.static('./public/images'));
 
-// Setup route
-app.use('/videos', videosRoute);
+app.use('/videos', videosRoute); 
 
-// start Express on port 5000
-app.listen(5000, () => {
-    console.log('Server Started on http://localhost:5000');
+const PORT = 5000;
+app.listen(PORT, () => {
+    console.log(`Server Started on http://localhost:${PORT}`);
     console.log('Press CTRL + C to stop server');
 });
