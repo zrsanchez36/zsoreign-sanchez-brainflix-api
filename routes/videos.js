44c503143e9fc29b5f4441d6c015f966dee5f57b
Zsoreign <zrsanchez36@gmail.com>
2023-10-02 12:13:28 -0400

    Added comments

--- routes/videos.js
+++ routes/videos.js
@@ -43,7 +43,27 @@ router.post('/', (req, res) => {
         title,
         description,
         image: '/images/default-image.jpg',
-        comments: [],
+        comments: [{
+            "id": uuid.v4(),
+            "name": "Micheal Lyons",
+            "comment": "They BLEW the ROOF off at their last event, once everyone started figuring out they were going. This is still simply the greatest opening of an event I have EVER witnessed.",
+            "likes": 0,
+            "timestamp": 1628522461000
+          },
+          {
+            "id": uuid.v4(),
+            "name": "Gary Wong",
+            "comment": "Every time I see him shred I feel so motivated to get off my couch and hop on my board. He’s so talented! I wish I can ride like him one day so I can really enjoy myself!",
+            "likes": 0,
+            "timestamp": 1626359541000
+          },
+          {
+            "id": uuid.v4(),
+            "name": "Theodore Duncan",
+            "comment": "How can someone be so good!!! You can tell he lives for this and loves to do it every day. Every time I see him I feel instantly happy! He’s definitely my favorite ever!",
+            "likes": 0,
+            "timestamp": 1626011132000
+          }],
     };
     videos.push(newVideo);
     fs.writeFileSync(videosPath, JSON.stringify(videos, null, 4));
